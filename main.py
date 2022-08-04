095fa63a8cfdda1d0e4faa37defe968d0dfd9c59
Miranda X Morris <88257891+mxm32@users.noreply.github.com>
2022-08-04 19:27:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,20 @@
-import requests
-from bs4 import BeautifulSoup
-import click
-from transformers import pipeline
+from flaml import AutoML
+from sklearn.datasets import load_iris
 
-
-def translate(article_text):
-    article_sentences = article_text.split(".")
-    output = ""
-    translator = pipeline(model="Helsinki-NLP/opus-mt-en-zh")
-    for sentence in article_sentences:
-        translation = translator(sentence)
-        output += list(translation[0].values())[0]
-    return output
-
-
-@click.command()
-@click.option(
-    "--url",
-    type=str,
-    default="https://en.wikipedia.org/wiki/Artificial_intelligence_industry_in_China",
-)
-def get_and_translate(url):
-    """
-    It takes a URL, downloads the article text, and translates it to Mandarin
-    """
-
-    # translate
-    article_text = get_article_text(url)
-    translation = translate(article_text)
-    print("Translation in Mandarin: ")
-    print(translation)
-
-
-def get_article_text(article_url):
-    # Get article
-    page = requests.get(article_url)
-    soup = BeautifulSoup(page.content, "html.parser")
-
-    # Extract body text
-    body_text = soup.find_all("p")
-    body_text = [i.text for i in body_text]
-    article_text = " ".join(body_text)
-    return article_text
-
-
-if __name__ == "__main__":
-    get_and_translate()  # pylint: disable=no-value-for-parameter
+# Initialize an AutoML instance
+automl = AutoML()
+# Specify automl goal and constraint
+automl_settings = {
+    "time_budget": 1,  # in seconds
+    "metric": 'accuracy',
+    "task": 'classification',
+    "log_file_name": "iris.log",
+}
+X_train, y_train = load_iris(return_X_y=True)
+# Train with labeled input data
+automl.fit(X_train=X_train, y_train=y_train,
+           **automl_settings)
+# Predict
+print(automl.predict_proba(X_train))
+# Print the best model
+print(automl.model.estimator)
